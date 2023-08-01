0de923b414798f2be1df76bd7006d6c03f282528
Payal Samal <payals@essbbsr.esspl.com>
2023-08-01 22:40:50 +0530

    cors fix

--- src/main/java/com/make/assure/MakeAssureApplication.java
+++ src/main/java/com/make/assure/MakeAssureApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@CrossOrigin(origins = "http://localhost:3000") 
 public class MakeAssureApplication {
 
 	public static void main(String[] args) {
